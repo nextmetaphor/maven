2d288f7d5d6ae87a94638bdf094969ea55b4b04f
Paul Tatham <paul@nextmetaphor.com>
2014-01-25 12:43:14 +0000

    Initial commit.

---
+++ CustomMavenLifecycle/CustomLifecyclePlugin/src/main/java/com/nextmetaphor/plugin/mojo/CustomLifecyclePluginPhase1Mojo.java
@@ -0,0 +1,15 @@
+package com.nextmetaphor.plugin.mojo;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+/**
+* Forks a zip lifecycle.
+* @goal phase1Goal
+*/
+public class CustomLifecyclePluginPhase1Mojo extends AbstractMojo {
+  public void execute() throws MojoExecutionException {
+    getLog().info("Doing Phase 1 stuff. Oh yeah baby.");
+  }
+}
